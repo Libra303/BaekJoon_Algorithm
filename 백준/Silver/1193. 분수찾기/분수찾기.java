b153ce811f80b0f3a192d0aa243ad53e6de546bf
Libra <jihyuk8778@gmail.com>
2023-04-06 11:43:44 +0900

    [Silver V] Title: 분수찾기, Time: 124 ms, Memory: 14204 KB -BaekjoonHub

--- 백준/Silver/1193. 분수찾기/분수찾기.java
+++ 백준/Silver/1193. 분수찾기/분수찾기.java
@@ -5,9 +5,11 @@ public class Main {
     public static void main(String[] args) throws Exception {
         //초기화
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
         int n = Integer.parseInt(br.readLine());
         int x = 1, y = 1, count = 0, sum = 0;
 
+        //로직
         for(int i = 1;sum < n;i++){
             sum = (i*(i+1))/2;
             count++;
@@ -24,8 +26,9 @@ public class Main {
             y = count - temp;
         }
         
-
-        System.out.println(x+"/"+ y);
+        //출력
+        sb.append(x).append("/").append(y);
+        System.out.println(sb);
     }
 }
 
