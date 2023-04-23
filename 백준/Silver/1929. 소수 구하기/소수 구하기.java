5f8fe36a7209aa73fb4468dad30c7e7e8e0017c9
Libra <jihyuk8778@gmail.com>
2023-04-23 20:28:07 +0900

    [Silver III] Title: 소수 구하기, Time: 480 ms, Memory: 22144 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.java
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -10,22 +10,20 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         StringBuilder sb = new StringBuilder();
         int start = Integer.parseInt(st.nextToken());
-        if(start == 1) start++;
         int end = Integer.parseInt(st.nextToken());
-        
+        if(start == 1) start++;
+
         //로직
+        outer :
         for(int i = start; i <= end; i++){
-            if(isPrime(i)) sb.append(i).append("\n");
+            for(int j = 2; j <= Math.sqrt(i); j++){
+                if(i % j == 0) continue outer;
+            }
+            sb.append(i).append("\n");
         }
 
         //출력
         System.out.println(sb);
     }
 
-    public static boolean isPrime(int n){
-        for(int i = 2; i <= Math.sqrt(n); i++){
-            if(n % i == 0) return false;
-        }
-        return true;
-    }
 }
