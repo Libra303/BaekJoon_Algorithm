3a0b500c3ff9f3c2a5615b9368c4343130a1cd86
Libra <jihyuk8778@gmail.com>
2023-04-26 20:25:57 +0900

    [Bronze IV] Title: 베라의 패션, Time: 124 ms, Memory: 14104 KB -BaekjoonHub

---
+++ 백준/Bronze/15439. 베라의 패션/베라의 패션.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        //초기화
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        //츨력
+        System.out.println(n*(n-1));
+    }
+}
